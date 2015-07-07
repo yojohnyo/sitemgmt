a1252d3247b0963a2f10dd6947c99f56aec34ecc
John Starr <yojohnyo1@gmail.com>
2015-07-06 22:16:41 -0500

    Sub name shows on folder form and ID stored in DB

--- addfolder.php
+++ addfolder.php
@@ -11,21 +11,51 @@ include'includes/includeFunctions.php';
 //check to see if it needs to display add subscription form
 
 $message = '';
+$subInfo = array();
+$subInput = '';
 
+$sql = "SELECT * FROM SUBSCRIPTIONS";
+$conn = dbConnect();
+$result = $conn->query($sql);
+if ($result->num_rows > 0){
+  while($row = $result->fetch_assoc()) {
+    //var_dump($row);
+    $subInfo[$row['id']]=$row['subscriptionName'];
+    $subInput .='<input type="radio" name="subID" value="'.$row['id'].'">'.$row['subscriptionName'].'<br>';
+  }
+
+  //print $subInput;
+  //var_dump($subInfo);
+  } else {
+  echo "No results returned";
+}
+connectClose($conn);
+
+//If first time on page, render the page
 if (!isset($_REQUEST) || count($_REQUEST)==0) {
   include 'addfolder.html';
 } else {
 
   $siteName = $_POST['folderName'];
+  
+  // Check for case where no radio button selected
+  if (!isset($_POST['subID'])) {
+    $subID =-1;
+  } else {
+    $subID = $_POST['subID'];
+  }
   $conn = dbConnect();
+  //Verify the folder doesn't already exist
   if (subExists($conn, $siteName, 'folderName', 'sitefolders')) {
-      $message = 'That siteFolder already exists';
-  } else {  
-    $sql = "INSERT INTO sitefolders (folderName) VALUES (?)";
+      $message = 'That siteolder already exists';
+  } elseif ($siteName == '' || $subID == -1) {
+      $message = 'That was not a valid entry';
+  } else {
+    $sql = "INSERT INTO sitefolders (folderName, subscriptionsID) VALUES (?,?)";
 
   //print $sql;
     $stmt = $conn->prepare($sql);
-    $stmt->bind_param("s", $siteName);
+    $stmt->bind_param("ss", $siteName, $subID);
     
     if ($stmt->execute()==TRUE) {
       //$last_id = $conn->insert_id; 
