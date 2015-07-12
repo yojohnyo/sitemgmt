1ec0dda4f96fc11ceec2912616a6c63b4803ad23
John Starr <yojohnyo1@gmail.com>
2015-07-11 23:43:15 -0500

    Add styling

--- index.php
+++ index.php
@@ -1,6 +1,5 @@
 <?php
-
-/* 
+/*
  * To change this license header, choose License Headers in Project Properties.
  * To change this template file, choose Tools | Templates
  * and open the template in the editor.
@@ -12,18 +11,37 @@ print '
         <title>Site Management Tool</title>
         <meta charset="UTF-8">
         <meta name="viewport" content="width=device-width, initial-scale=1.0">
+        <link rel="stylesheet" type="text/css" href="css/the.css" media="screen" />
         <script src="includes/javascriptincludes.js"></script>';
-        if (isset($jsfile)) {
-          print $jsfile;
-        }
-print '
-    </head>
-    <body>
-        <div>';
-if (isset ($message)) print $message;
-include"headerfile.html";
+if (isset($jsfile)) {
+  print $jsfile;
+}
+?>
 
-print '</div>
-        
-    </body>
-</html>';
+</head>
+<body>
+  <div id="wrap">
+    <div id="top">
+      <h2><a href="index.php">Site Management Tool</a></h2>
+      <nav>
+        <div id="menu">
+          <ul>
+            <li><a href="index.php">Home</a></li>
+            <li><a href="addSubscription.php">Add Subscription</a></li>
+            <li><a href="addfolder.php">Add Folder</a></li>
+            <li><a href="displayFolders.php">Display folder</a></li>
+          </ul>
+        </div>
+      </nav>
+
+    </div>      
+    <div id="message">
+      <?php
+      if (isset($message))
+        print $message;
+      ?>
+    </div>
+
+
+</body>
+</html>
